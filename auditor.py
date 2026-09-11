be017157506581075bef3c7ae7c8f56e432f8866
Anteng <Antenglim@gmail.com>
2026-09-11 15:05:24 +0800

    Task 3

--- auditor.py
+++ auditor.py
@@ -1,4 +1,8 @@
 stock_quantity = 0
 while stock_quantity !="quit":
     stock_quantity = input("Enter the stock quantity")
+    if stock_quantity.isdigit()== True:
+        stock_quantity = int(stock_quantity)
+        print(f"Stock quantity entered: {stock_quantity}")
+        break
 
