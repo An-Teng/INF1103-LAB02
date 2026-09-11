83ada8721931c285a9824b3fdfd6b1fe3fb4e846
Anteng <Antenglim@gmail.com>
2026-09-11 15:27:28 +0800

    Task 6

--- auditor.py
+++ auditor.py
@@ -1,12 +1,13 @@
 stock_quantity = 0
+Inventory=0
 while stock_quantity !="quit":
     stock_quantity = input("Enter the stock quantity: ")
-    print(int(stock_quantity))
     if int(stock_quantity) < 0:
                 print("Invalid input. Stock quantity cannot be negative")
     elif stock_quantity.isnumeric()== True:
             stock_quantity = int(stock_quantity)
             print("Stock quantity entered:", stock_quantity)
+            Inventory += stock_quantity
     elif stock_quantity.isdigit() == False:
         print("Invalid input. Please enter a valid stock quantity")
     
