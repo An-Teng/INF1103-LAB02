fc5572771f0ea07d80744feaf348cd23f1e3a825
Anteng <Antenglim@gmail.com>
2026-09-11 15:00:10 +0800

    Task 2

--- auditor.py
+++ auditor.py
@@ -1,3 +1,4 @@
 stock_quantity = 0
-while stock_quantity!="quit":
-    stock_quantity = input("Enter the stock quantity: ")
+while stock_quantity !="quit":
+    stock_quantity = input("Enter the stock quantity")
+
