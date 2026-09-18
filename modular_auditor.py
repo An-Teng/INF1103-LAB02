592545f338511421112e6186a712c151d38c02d7
Anteng <Antenglim@gmail.com>
2026-09-18 15:52:58 +0800

    get valid

---
+++ modular_auditor.py
@@ -0,0 +1,21 @@
+stock_quantity = 0
+Failed=0
+Inventory=0
+def get_valid_input(Failed):
+            stock_quantity = input("Enter the stock quantity: ")
+            try:
+                    if stock_quantity=="quit":
+                        return stock_quantity,Failed
+                    if int(stock_quantity) < 0:
+                        print("Invalid input. Stock quantity cannot be negative")
+                        Failed += 1
+                    else:
+                            return stock_quantity,Failed
+            except ValueError:
+                    print("Invalid input. Please enter a valid stock quantity")
+                    Failed += 1
+                    return get_valid_input(Failed)
+while stock_quantity !="quit":
+ stock_quantity,Failed=get_valid_input(Failed)
+ print("Quantity entered:", stock_quantity)
+ print("Number of Failed entries:", Failed)
